c36a6822630ea2ea3cf1c8f6dcbc537b9886bafc
joaogrbm <jgrbm66@gmail.com>
2021-08-23 19:15:57 -0300

    primeiro commit

---
+++ backend/src/main/java/com/joaogmelo/employeemanager/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.joaogmelo.employeemanager.exception;
+
+public class UserNotFoundException extends RuntimeException{
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ backend/src/test/java/com/joaogmelo/employeemanager/EmployeemanagerApplicationTests.java
@@ -0,0 +1,10 @@
+package com.joaogmelo.employeemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeemanagerApplicationTests {
+
+
+}
